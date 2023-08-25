110e1ea97425b860985fdefb9361218178b36747
lc.chai <lc.chai.sg@gmail.com>
2023-08-26 00:01:38 +0800

    longines products graphql on SKU

--- longines.js
+++ longines.js
@@ -1,22 +1,44 @@
-const axios = require('axios')
-const cheerio = require('cheerio')
-const url = "https://www.longines.com/en-us"
+const axios = require('axios');
+const cats = require('./const_cats');
+const graphql = "https://www.longines.com/api/graphql";
 
-const indexing = async (input) => {
-    const { client, entry, base, interval } = input
-    const source = "official"
-    const lang = "en"
-    const brand = "Longines"
-    const brandId = 220020
-    const result = { source, lang, brand, brandId, collections:[], watches:{} }
-    const col = []
-    const { data } = await client.get(entry)
-    if (data) {
-        const $ = cheerio.load(data)
+const categoriesqry = `query{categories{items{uid}}}`;
+let productsqry = `query{products(pageSize:2000,filter:{category_uid:{eq:"Mg=="}}){total_count items{categories{uid}}}}`;
+const categories = [];
+    
+(async() => {
+    for (const cat of cats) {
+        console.log(`uid : ${cat.uid}`);
+        productsqry = `query{products(pageSize:2000,filter:{category_uid:{eq:"${cat.uid}"}}){total_count items{product_sku uid}}}`;
+        const { data } = await axios.post(graphql, {query:productsqry});
+        for (const d of data.data.products.items) {
+            console.log(d);
+        }
+        process.exit(0);
     }
-}
 
-(async () => {
-    const { data } = await axios.get(url)
-    console.log(data)
-})()
+    // const { data } = await axios.post(graphql, {query: categoriesqry});
+    //     if (data && data.data && data.data.categories && data.data.categories.items) {
+    //     if (data.data.categories.items.length > 0) {
+    //         for (const i of data.data.categories.items) {
+    //             productsqry = `query{products(pageSize:2000,filter:{category_uid:{eq:"${i.uid}"}}){total_count items{categories{uid name}}}}`;
+    //             const { data } = await axios.post(graphql, {query: productsqry});
+    //             if (data && data.data && data.data.products && data.data.products.items) {
+    //                 data.data.products.items.forEach(r => {
+    //                     if (r.categories && r.categories.length > 0) {
+    //                         r.categories.forEach(i => {
+    //                             // if (categories.indexOf(i.uid) < 0)
+    //                             //     categories.push(i.uid);
+    //                             if (!categories.some(item => item.uid === i.uid))
+    //                                 categories.push(i);
+    //                         })
+    //                     }
+    //                 })
+    //             }
+    //         }
+    //     }
+    // }
+    // categories.sort().forEach(u => {
+    //     console.log(u);
+    // })
+})();
