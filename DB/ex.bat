f9024b8eb8653c3bf5e2b6272aff16d3753bf730
Leong Choi <leongchoi.chai@imperialtreasure.com>
2023-08-31 17:08:01 +0800

    db export

---
+++ DB/ex.bat
@@ -0,0 +1,3 @@
+mongoexport --db watches --collection brands --out brands.json
+mongoexport --db watches --collection collections --out collections.json
+mongoexport --db watches --collection products --out products.json
