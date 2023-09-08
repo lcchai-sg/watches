65aad1e37979e8cf70a5a8d773e4d709d4f34141
Leong Choi <leongchoi.chai@imperialtreasure.com>
2023-09-08 16:19:55 +0800

    update brands and some translations

---
+++ translations.js
@@ -0,0 +1,43 @@
+EN                              SC                      IT
+power reserve                   动力储存
+dial                            表盘
+hands                           指针                    LANCETTE
+bracelet                        表链
+limited edition                 限量版
+distinctions                    独特点
+case                            表壳
+Stainless steel                 精钢                    Acciaio Inox
+Sapphire crystal                蓝宝石水晶玻璃
+anti-reflective coating         经防眩处理
+Light blue PVD coloured         浅蓝色PVD镀膜
+mainplate and bridges           主夹板和桥板
+double folding clasp            双折叠表扣 
+Width lugs/buckle               表耳宽度/带扣
+diameter                        直径
+height                          厚度
+lug-to-lug                      表耳间距
+water resistance                防水性能                RESISTENZA ACQUA
+ATM                             大气压
+mm                              毫米
+Black minute ring               黑色分钟圈刻度
+small-second dial               小秒盘
+luminescense                    夜光涂层
+Brushed                                                 Spazzolata
+CASE SIZE                                               DIMENSIONE CASSA
+CROWN                                                   CORONA
+Screw-Down Crown System                                 Chiusura a Vite
+BEZEL                                                   GHIERA
+Black IP                                                IP Nero
+Interchangeable with Brera screws                       Intercambiabile con viti disegno Brera
+High Luminescence                                       Alta Luminescenza
+STRAP                                                   CINTURINO
+clasp                           表扣                    CHIUSURA
+Deployant                                               Deployant 
+MOVEMENT                                                MOVIMENTO
+Chronograph                                             Cronografo 
+Japanese                                                Giapponese 
+
+
+
+
+
